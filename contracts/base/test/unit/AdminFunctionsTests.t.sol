dabfe8297714a17243c5e486712a2b12fb49797b
Arav Bhivgade <bhivgadearav@gmail.com>
2026-07-25 20:57:52 +0530

    feat: added tests for whitelist check when claiming rewards

--- contracts/base/test/unit/AdminFunctionsTests.t.sol
+++ contracts/base/test/unit/AdminFunctionsTests.t.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.28;
 
 import {Test, console} from "../../lib/forge-std/src/Test.sol";
-import {LockType, LockInfo} from "../../src/BuffCat.sol";
+import {BuffCatUpgradeable, LockType, LockInfo} from "../../src/BuffCat.sol";
 import {TestSetUp} from "./lib/TestSetUp.sol";
 import "../../lib/openzeppelin-contracts-upgradeable/contracts/utils/PausableUpgradeable.sol";
 import "../../lib/openzeppelin-contracts-upgradeable/contracts/access/OwnableUpgradeable.sol";
@@ -103,6 +103,65 @@ contract AdminFunctionsTests is TestSetUp {
             balanceAfterUnlocking,
             "2: Wrong Balance"
         );
+
+        vm.stopPrank();
+    }
+
+    function testSuddenBlacklist() public {
+        vm.startPrank(user2);
+
+        buffCat.lockAssets(
+            address(token2),
+            LOCK_AMOUNT,
+            30,
+            LockType.FLEXIBLE,
+            address(0)
+        );
+
+        vm.stopPrank();
+
+        vm.startPrank(user1);
+
+        buffCat.lockAssets(
+            address(token1),
+            LOCK_AMOUNT,
+            30,
+            LockType.FLEXIBLE,
+            address(0)
+        );
+
+        vm.warp(block.timestamp + 1 days);
+
+        address[] memory tokens = new address[](1);
+        tokens[0] = address(token2);
+        buffCat.claimRewards(tokens, 0, 0);
+
+        uint256 expectedRewards = 3008333333333333;
+        uint256 token2Balance = token2.balanceOf(user1);
+        assertEq(
+            token2Balance,
+            INITIAL_BALANCE + expectedRewards,
+            "Wrong balance after claiming"
+        );
+
+        vm.stopPrank();
+
+        vm.startPrank(owner);
+
+        address[] memory blacklist = new address[](1);
+        blacklist[0] = address(token1);
+        buffCat.blacklistTokens(blacklist);
+
+        vm.stopPrank();
+
+        vm.startPrank(user1);
+
+        vm.warp(block.timestamp + 2 days);
+
+        vm.expectRevert(
+            abi.encodeWithSelector(BuffCatUpgradeable.InvalidToken.selector)
+        );
+        buffCat.claimRewards(tokens, 0, 0);
 
         vm.stopPrank();
     }
