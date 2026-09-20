a633241cf47fc7e2437067a80e5e339ea7b9dce5
Arav Bhivgade <bhivgadearav@gmail.com>
2026-09-20 19:27:50 +0530

    feat: deployed buffcat on ethereum & added contract address in envVariables

--- apps/web/src/constants/blockchains.ts
+++ apps/web/src/constants/blockchains.ts
@@ -1,13 +1,13 @@
 import { Blockchain } from '@/types/global';
 
 export const blockchains: Blockchain[] = [
-  // {
-  //   chainId: 1,
-  //   id: 'eth',
-  //   name: 'Ethereum',
-  //   logoUrl: 'https://s2.coinmarketcap.com/static/img/coins/64x64/1027.png',
-  //   isSupported: true,
-  // },
+  {
+    chainId: 1,
+    id: 'eth',
+    name: 'Ethereum',
+    logoUrl: 'https://s2.coinmarketcap.com/static/img/coins/64x64/1027.png',
+    isSupported: true,
+  },
   {
     chainId: 8453,
     id: 'base',
--- apps/web/src/lib/envVariables.ts
+++ apps/web/src/lib/envVariables.ts
@@ -4,7 +4,7 @@ export const envVariables: {
   buffcatContract: Record<SupportedBlockchain, string>;
 } = {
   buffcatContract: {
-    eth: '',
+    eth: '0x31eCd0a6d66263Bc1d0150a0e0eEfc9204D6A391',
     base: '0x31eCd0a6d66263Bc1d0150a0e0eEfc9204D6A391',
     solana: '',
   },
